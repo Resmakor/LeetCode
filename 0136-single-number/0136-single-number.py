ae7f89995a4eaffa3b36a2ab377b7257ce3a8c7a
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-02-12 19:19:56 +0100

    Time: 109 ms (39.34%), Space: 13.9 MB (75.44%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,21 @@
 class Solution(object):
     def singleNumber(self, nums):
-        for element in nums:
-            if nums.count(element) == 1:
-                return element
+        nums.sort()
+        length = len(nums)
+        for i in range(length + 1):
+            if i > 0 and i < length - 1 and length > 3:
+                if nums[i] != nums[i + 1] and nums[i + 1] != nums[i + 2]:
+                    return nums[i + 1]
+                else:
+                    i += 1
+            if i == 0:
+                if length == 1:
+                    return nums[i]
+                else:
+                    if nums[i] != nums[i + 1]:
+                        return nums[i]
+            if i == length - 2:
+                if nums[i] != nums[i + 1]:
+                    return nums[i + 1]
         
         
