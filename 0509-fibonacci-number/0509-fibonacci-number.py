5ae271664f212c9c4ebe84c58685e71087260222
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-02-14 03:31:39 +0100

    Time: 16 ms (66.52%), Space: 11.7 MB (58.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,16 @@
 class Solution(object):
     def fib(self, n):
+        a = 0
+        b = 1
         if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        return self.fib(n - 1) + self.fib(n - 2)
+            return a
+        if n == 1:
+            return b
+        counter = 1
+        while (counter != n):
+            tmp = b
+            b += a
+            a = tmp
+            counter += 1
+        return b
+        
