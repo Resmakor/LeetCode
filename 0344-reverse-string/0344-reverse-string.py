31947eabc7c2563cab172723649cd7a50d84e383
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-03-01 16:02:57 +0100

    Time: 128 ms (77.78%), Space: 19.2 MB (96.35%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        half_of_s_size = len(s) / 2
-        for i in range(half_of_s_size):
-            s[i], s[-1 - i] = s[-1 - i], s[i] 
+        s.reverse() 
