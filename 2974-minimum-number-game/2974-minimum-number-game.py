0ca94c95d6440d1d322e585751adb1e26c21e84e
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-03-02 14:16:04 +0100

    Time: 41 ms (17.41%), Space: 11.7 MB (67.25%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numberGame(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        size = len(nums)
+        for i in range(0, size, 2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
