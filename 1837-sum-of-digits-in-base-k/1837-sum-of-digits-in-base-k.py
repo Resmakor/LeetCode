5f7a12c2f2af903193fdc6ca88c9aa6446978f86
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-03-09 17:53:34 +0100

    Time: 12 ms (67.78%), Space: 11.6 MB (43.33%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
@@ -1,23 +1,12 @@
 class Solution(object):
-    def convert(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: str
-        """
-        new_Base = ""
-        while n != 0:
-            new_Base += str(n % k)
-            n //= k
-        return new_Base
     def sumBase(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: int
         """
-        new_Base = self.convert(n, k)
-        sum = 0
-        for digit in new_Base:
-            sum += int(digit)
-        return sum
+        new_Base_sum = 0
+        while n != 0:
+            new_Base_sum += n % k
+            n //= k
+        return new_Base_sum
