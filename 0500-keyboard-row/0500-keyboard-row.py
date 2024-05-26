0dd1e332fa314adcd4bd7dc3b133ebc337016731
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-05-26 17:53:41 +0200

    Time: 8 ms (90.28%), Space: 11.7 MB (60.19%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        first_row = "qwertyuiop"
+        second_row = "asdfghjkl"
+        third_row = "zxcvbnm"
+        output = []
+        first_flag = True
+        second_flag = True
+        third_flag = True
+        for word in words:
+            for letter in word:
+                if letter.lower() not in first_row:
+                    first_flag = False
+            if first_flag == True:
+                output.append(word)
+            else:
+                for letter in word:
+                    if letter.lower() not in second_row:
+                        second_flag = False
+                if second_flag == True:
+                    output.append(word)
+                else:
+                    for letter in word:
+                        if letter.lower() not in third_row:
+                            third_flag = False
+                    if third_flag == True:
+                        output.append(word)
+            first_flag = True
+            second_flag = True
+            third_flag = True
+        return output
