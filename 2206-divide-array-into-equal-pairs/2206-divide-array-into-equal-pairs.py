b1a74ff9f485ff240ca32469e74fadccdcdc5345
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-06-27 14:39:23 +0200

    Time: 61 ms (69.28%), Space: 11.6 MB (85.62%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(0, len(nums), 2):
+            if nums[i] != nums[i + 1]:
+                return False
+        return True
