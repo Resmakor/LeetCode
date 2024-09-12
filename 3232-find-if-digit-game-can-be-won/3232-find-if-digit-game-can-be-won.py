b61929b7e843498f67e5353beb43bfd11b96521d
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-09-12 17:26:53 +0200

    Time: 71 ms (5.49%), Space: 11.5 MB (72.56%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canAliceWin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        single_sum = 0
+        double_sum = 0
+        for element in nums:
+            if len(str(element)) == 2:
+                double_sum += element
+            else:
+                single_sum += element
+        return single_sum != double_sum
