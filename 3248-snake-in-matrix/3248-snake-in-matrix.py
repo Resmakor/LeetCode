0c6c2382ca7ee51272c8be5cb2d2f7e5d3d464d7
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-10-18 11:57:07 +0200

    Time: 129 ms (5.46%), Space: 11.5 MB (98.08%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def finalPositionOfSnake(self, n, commands):
+        """
+        :type n: int
+        :type commands: List[str]
+        :rtype: int
+        """
+        matrix, counter = [], 0
+        for i in range(n):
+            temp_list = []
+            for j in range(n):
+                temp_list.append(counter)
+                counter += 1
+            matrix.append(temp_list)
+        print(matrix)
+        i, j = 0, 0
+        for command in commands:
+            if command == "UP":
+                i -= 1
+            if command == "DOWN":
+                i += 1
+            if command == "RIGHT":
+                j += 1
+            if command == "LEFT":
+                j -= 1
+        return matrix[i][j]
