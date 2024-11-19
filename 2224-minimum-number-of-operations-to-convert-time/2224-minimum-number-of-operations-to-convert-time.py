70d181e1cef9eb9226050247f534353ad2c480f6
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-11-19 19:52:31 +0100

    Time: 0 ms (100.00%), Space: 11.5 MB (96.30%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -9,9 +9,8 @@ class Solution(object):
         current_minutes = int(current[0:2]) * 60 + int(current[3:5])
         correct_minutes = int(correct[0:2]) * 60 + int(correct[3:5])
         difference = correct_minutes - current_minutes
-        while difference != 0:
-            for i in (60, 15, 5, 1):
-                if difference // i != 0:
-                    output += difference // i
-                    difference %= i
+        for i in (60, 15, 5, 1):
+            if difference // i != 0:
+                output += difference // i
+                difference %= i
         return output
