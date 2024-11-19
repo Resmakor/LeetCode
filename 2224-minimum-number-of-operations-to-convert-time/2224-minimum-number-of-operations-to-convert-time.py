bff22c62033cf5ec31598a3812252e0f3d76574b
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-11-19 19:39:37 +0100

    Time: 0 ms (100.00%), Space: 11.8 MB (9.26%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convertTime(self, current, correct):
+        """
+        :type current: str
+        :type correct: str
+        :rtype: int
+        """
+        output = 0
+        current_minutes = int(current[0:2]) * 60 + int(current[3:5])
+        correct_minutes = int(correct[0:2]) * 60 + int(correct[3:5])
+        difference = correct_minutes - current_minutes
+        while difference != 0:
+            for i in (60, 15, 5, 1):
+                if difference // i != 0:
+                    output += difference // i
+                    difference %= i
+        return output
