26e4a0a846292372f37843af0965c47fe815ff38
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-11-21 20:44:46 +0100

    Time: 79 ms (6.38%), Space: 11.8 MB (7.80%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums) - 1):
+        nums_size = len(nums)
+        for i in range(nums_size):
+            for j in range(i + 1, nums_size - 1):
                 if nums[i] + nums[i + 1] == nums[j] + nums[j + 1]:
                     return True
         return False
