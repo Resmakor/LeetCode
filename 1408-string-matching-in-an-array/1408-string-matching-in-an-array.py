61babc0df77056946f0dc6d1be66fac70615d9c9
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-12-17 16:19:43 +0100

    Time: 0 ms (100.00%), Space: 12.6 MB (9.30%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        output = []
+        #words.sort()
+        for i in range(len(words)):
+            for j in range(0, len(words)):
+                if i == j:
+                    continue
+                if words[i] in words[j]:
+                    output.append(words[i])
+                    break
+        return output
