da2d978ccd9abe5cbd235dfb25ba57e1324a7011
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-12-26 18:28:25 +0100

    Time: 85 ms (44.52%), Space: 38 MB (11.66%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,32 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        self.hashSet = [False] * (1000001)
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.hashSet[key] = True
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.hashSet[key] = False
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        return self.hashSet[key]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
