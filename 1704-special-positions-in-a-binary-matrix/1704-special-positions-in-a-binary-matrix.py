a19acc896f93b47eda4297c8f0513bda79a831a0
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2025-06-23 19:51:30 +0200

    Time: 9 ms (58.82%), Space: 12.7 MB (50.98%) - LeetHub

--- 1704-special-positions-in-a-binary-matrix/1704-special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/1704-special-positions-in-a-binary-matrix.py
@@ -7,7 +7,6 @@ class Solution(object):
         output = 0
         row_sum = [sum(row) for row in mat]
         column_sum = [sum(mat[i][j] for i in range(len(mat))) for j in range(len(mat[0]))]
-        print(column_sum)
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 1 and row_sum[i] == 1 and column_sum[j] == 1:
