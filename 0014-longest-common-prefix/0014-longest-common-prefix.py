51f302ab10a7c608b9d52824d0ff55ce67b13005
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2025-09-13 17:31:35 +0200

    Time: 1 ms (43.37%), Space: 12.4 MB (91.92%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        output = ""
+        strs = sorted(strs)
+        first, last = strs[0], strs[-1]
+        print(strs)
+        for i in range(len(first)):
+            if first[i] != last[i]:
+                return output
+            output += first[i]
+        return output
