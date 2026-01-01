65ec476a83d670f71f1b8ff58aad6a3d12bfbc19
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2026-01-01 16:04:33 +0100

    Time: 0 ms (100%), Space: 17.1 MB (99.34%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def plusOne(self, digits):
-        if len(digits) == 0:
-            return [1]
-        elif digits[-1] == 9:
-            return self.plusOne(digits[:-1]) + [0]
-        else:
-            digits[-1] += 1
-            return digits
-                
-                
-        
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)):
+            if digits[len(digits) - i - 1] == 9:
+                digits[len(digits) - i - 1] = 0
+            else:
+                digits[len(digits) - i - 1] += 1
+                return digits
+        return [1] + digits
