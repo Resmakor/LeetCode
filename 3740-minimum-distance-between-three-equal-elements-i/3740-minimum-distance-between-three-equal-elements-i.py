010e4fc8ff177231a7b4bed6d0587b21becfde70
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2026-02-21 12:43:32 +0100

    Time: 0 ms (100%), Space: 19.5 MB (20.21%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -2,14 +2,17 @@ class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         minim = math.inf
         hasGoodTuple = False
-        for i in range(len(nums) - 2):
-            for j in range(i + 1, len(nums) - 1):
-                if nums[j] != nums[i]:
-                    continue
-                for k in range(j + 1, len(nums)):
-                    if nums[j] == nums[k]:
-                        hasGoodTuple = True
-                        minim = min(abs(i - j) + abs(j - k) + abs(k - i), minim)
-        if not hasGoodTuple:
-            return -1
-        return minim
+        values = {}
+        for index, value in enumerate(nums):
+            if value not in values:
+                values[value] = [index]
+            else:
+                values[value].append(index)
+            if len(values[value]) >= 3:
+                hasGoodTuple = True
+                i, j, k = values[value][-3], values[value][-2], values[value][-1]
+                current_value = abs(i - j) + abs(j - k) + abs(k - i)
+                minim = min(minim, current_value)
+        if hasGoodTuple:
+            return minim
+        return -1
